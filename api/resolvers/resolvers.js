7ff10d222786aafe6abd3989a4f5f5e1658681aa
Tom Radford <tom@theradford.com>
2022-10-05 14:25:56 +0200

    Feat: Web setup with create-next-app

--- api/resolvers/resolvers.js
+++ api/resolvers/resolvers.js
@@ -261,11 +261,11 @@ const resolvers = {
 				userThatUpdated: context.currentUser,
 				prefs: prefs
 					? prefs.map((pref) => {
-						return {
-							pref: mongoose.Types.ObjectId(pref.id),
-							opts: pref.opts.map((opt) => mongoose.Types.ObjectId(opt)),
-						}
-					})
+							return {
+								pref: mongoose.Types.ObjectId(pref.id),
+								opts: pref.opts.map((opt) => mongoose.Types.ObjectId(opt)),
+							}
+					  })
 					: null,
 			})
 			return await listToAdd.save()
