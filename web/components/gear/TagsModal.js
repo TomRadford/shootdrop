4ca8784ea19a29cfa2eedcb196f1ad21538a1ce3
Tom Radford <tom@theradford.com>
2022-10-28 16:10:19 +0200

    wip

--- web/components/gear/TagsModal.js
+++ web/components/gear/TagsModal.js
@@ -6,12 +6,15 @@ import { Transition, Dialog } from "@headlessui/react"
 
 const TagsModal = ({ setTagsModalOpen, tagsModalOpen }) => {
   const [searchTerm, setSearchTerm] = useState("")
+  const [loading, setLoading] = useState(false)
   const me = useGetMe()
   const allTags = useQuery(ALL_TAGS, {
     variables: {
       tag: searchTerm,
     },
   })
+  console.log(allTags)
+
   const [editGearItem, editGearItemResult] = useMutation(EDIT_GEAR_ITEM)
   return (
     <Transition appear show={tagsModalOpen} as={Fragment}>
@@ -31,7 +34,6 @@ const TagsModal = ({ setTagsModalOpen, tagsModalOpen }) => {
         >
           <div className="fixed inset-0 bg-black bg-opacity-50" />
         </Transition.Child>
-
         <div className="fixed inset-0 overflow-y-auto md:ml-72">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
@@ -50,8 +52,53 @@ const TagsModal = ({ setTagsModalOpen, tagsModalOpen }) => {
                 >
                   Add a tag
                 </Dialog.Title>
-                <div className="mt-2 grid grid-cols-2 gap-2 sm:grid-cols-4">
-                  tag search
+                <div>
+                  <div className="mx-2 my-2">
+                    <input
+                      className="w-full bg-transparent text-white"
+                      placeholder="Search"
+                      value={searchTerm}
+                      onChange={({ target }) => setSearchTerm(target.value)}
+                    />
+                  </div>
+
+                  {allTags.loading ? (
+                    <div className="mx-auto">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#616060"
+                        className="h-4 w-4 animate-spin"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                        />
+                      </svg>
+                    </div>
+                  ) : (
+                    allTags.data && (
+                      <div className="inline-grid grid-flow-col-dense gap-2 ">
+                        {allTags.data.allTags.map((tag) => {
+                          return (
+                            <button
+                              onClick={(e) => {
+                                e.preventDefault()
+                                setTagsModalOpen(true)
+                              }}
+                              key={tag.id}
+                              className="flex items-center rounded bg-teal-600 px-2 py-1 text-sm "
+                            >
+                              {tag.name}
+                            </button>
+                          )
+                        })}
+                      </div>
+                    )
+                  )}
                 </div>
               </Dialog.Panel>
             </Transition.Child>
