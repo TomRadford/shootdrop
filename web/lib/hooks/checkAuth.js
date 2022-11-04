0461067a49027d35c5a6c9fb5211e07e3bf2d063
Tom Radford <tom@theradford.com>
2022-11-04 15:31:01 +0200

    fix: check auth no longer requires useEffect
    this prevents unneeded re-renders

--- web/lib/hooks/checkAuth.js
+++ web/lib/hooks/checkAuth.js
@@ -1,18 +1,16 @@
-import { useEffect } from "react"
 import { ME } from "../../lib/apollo/queries"
 import { useQuery } from "@apollo/client"
 import { useRouter } from "next/router"
 
 const useCheckAuth = () => {
-  const me = useQuery(ME)
   const router = useRouter()
-  useEffect(() => {
-    if (!me.loading) {
-      if (!me.data.me) {
-        router.push("/login")
-      }
-    }
-  }, [me.data])
+  const { loading, data } = useQuery(ME)
+  if (loading) {
+    return null
+  }
+  if (!data.me) {
+    router.push("/login")
+  }
 }
 
 export default useCheckAuth
