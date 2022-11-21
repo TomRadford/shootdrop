278a3b9a128c4845777127c2425921077da25c26
Tom Radford <tom@theradford.com>
2022-11-21 14:29:52 +0200

    to revert

--- web/components/drop/List.js
+++ web/components/drop/List.js
@@ -1,14 +1,19 @@
 import { useMutation, useQuery } from "@apollo/client"
-import { ADD_LIST } from "../../lib/apollo/queries"
+import { ADD_LIST, GET_LIST_ITEMS } from "../../lib/apollo/queries"
 import { AddButton } from "../AddCard"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
 import Card from "../Card"
 import useUserInDrop from "../../lib/hooks/userInDrop"
 import useGetMe from "../../lib/hooks/getMe"
+import Link from "next/link"
+
 const DropListInfo = ({ drop, category, listEntry }) => {
   const router = useRouter()
   const userInDrop = useUserInDrop(drop)
+  const getListItems = useQuery(GET_LIST_ITEMS, {
+    variables: { id: listEntry.id },
+  })
   const me = useGetMe()
   const [addList, { data, loading, error }] = useMutation(ADD_LIST)
   const handleAdd = (e) => {
@@ -30,15 +35,22 @@ const DropListInfo = ({ drop, category, listEntry }) => {
   return (
     <>
       {listEntry ? (
-        <div className="mx-auto w-80 sm:w-96">
-          <Card>
-            <div className="pb-13 flex flex-col gap-4 px-4 py-2 pb-[3.2rem]">
-              <h3 className="text-md pb-1 text-left font-semibold">
-                {`${category[0]}${category.slice(1).toLowerCase()}`}
-              </h3>
+        <Link href={`/list/${listEntry.id}`}>
+          <a>
+            <div className="mx-auto w-80 sm:w-96">
+              <Card>
+                <div className="pb-13 flex flex-col gap-2 px-4 py-2 pb-[3.2rem]">
+                  <h3 className="text-md text-left font-semibold">
+                    {`${category[0]}${category.slice(1).toLowerCase()}`}
+                  </h3>
+                  <p className="text-left text-sm font-light text-gray-300">
+                    {listEntry.comment}
+                  </p>
+                </div>
+              </Card>
             </div>
-          </Card>
-        </div>
+          </a>
+        </Link>
       ) : userInDrop && me ? (
         <AddButton title={category} onClick={handleAdd} />
       ) : null}
