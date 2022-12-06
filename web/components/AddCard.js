23d571586c3584c99076e73671f79e1d40a1a2ee
Tom Radford <tom@theradford.com>
2022-12-06 18:29:13 +0200

    feat: List create loader

--- web/components/AddCard.js
+++ web/components/AddCard.js
@@ -1,7 +1,8 @@
 import Link from "next/link"
 import Card from "./Card"
+import LoadingSpinner from "./LoadingSpinner"
 
-export const AddButton = ({ onClick, title }) => (
+export const AddButton = ({ onClick, title, loading }) => (
   <button className={`relative mx-auto w-80 sm:w-96`}>
     <Card>
       {title && (
@@ -10,20 +11,26 @@ export const AddButton = ({ onClick, title }) => (
         }${title.slice(1).toLowerCase()}`}</div>
       )}
       <div className="flex h-44 items-center justify-center" onClick={onClick}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={4}
-          stroke="currentColor"
-          className="h-8 w-8"
-        >
-          <path
-            strokeLinecap="square"
-            strokeLinejoin="square"
-            d="M12 4.5v15m7.5-7.5h-15"
-          />
-        </svg>
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={4}
+              stroke="currentColor"
+              className="h-8 w-8"
+            >
+              <path
+                strokeLinecap="square"
+                strokeLinejoin="square"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </>
+        )}
       </div>
     </Card>
   </button>
