53c7ecda067e04b7c5354e22b8cc73b0eaa45264
Tom Radford <tom@theradford.com>
2025-06-06 21:47:20 +0200

    add next link to button

--- web/components/elements/Button.tsx
+++ web/components/elements/Button.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
 import React, { forwardRef } from 'react'
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-	href?: undefined
-}
-
-type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+type SharedProps = {
+	className?: string
 	href?: string
 }
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & SharedProps
+
+type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & SharedProps
 
 //Input/output options
 type Overload = {
@@ -18,14 +20,22 @@ type Overload = {
 const hasHref = (props: ButtonProps | AnchorProps): props is AnchorProps =>
 	'href' in props
 
+const isRelativeHref = (href: string): boolean => href.startsWith('/')
+
 const style = `bg-size-200 bg-pos-0 hover:bg-pos-100 rounded bg-gradient-to-r from-sky-700 via-sky-800 to-sky-900 px-3 py-1 font-bold transition-all duration-500`
 
-const Button: Overload = (
-	props: ButtonProps | AnchorProps,
-	ref: React.Ref<string>
-) => {
+const Button: Overload = (props: ButtonProps | AnchorProps) => {
 	//render anchor
-	if (hasHref(props)) return <a {...props} className={style} />
+	if (hasHref(props))
+		return isRelativeHref(props.href) ? (
+			<Link href={props.href} className={style}>
+				{props.children}
+			</Link>
+		) : (
+			<a {...props} className={style}>
+				{props.children}
+			</a>
+		)
 	//render button
 	return <button {...props} className={style} />
 }
