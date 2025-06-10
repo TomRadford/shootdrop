f4b0bf301d491ca36afb0551562bc21fd4091f1f
Tom Radford <tom@theradford.com>
2025-06-10 18:03:22 +0200

    enhance codegen

--- web/codegen.ts
+++ web/codegen.ts
@@ -2,14 +2,27 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
 	schema: '../api/schema.ts',
-	documents: './lib/apollo/queries.ts',
+	documents: ['./**/*.{ts,tsx}', '!./__generated__/**'],
 	generates: {
-		'./__generated__/': {
-			preset: 'client',
-			documents: ['./**/*.{ts,tsx}', '!./__generated__'],
-			plugins: [],
-			presetConfig: {
-				gqlTagName: 'gql',
+		'./__generated__/graphql.ts': {
+			plugins: [
+				'typescript',
+				'typescript-operations',
+				'typescript-react-apollo',
+				'typescript-graphql-request',
+			],
+			config: {
+				withHooks: true,
+				withHOC: false,
+				withComponent: false,
+				exportFragmentSpreadSubTypes: true,
+				dedupeFragments: true,
+				skipTypename: false,
+				documentMode: 'documentNode',
+				strictScalars: true,
+				scalars: {
+					Date: 'Date',
+				},
 			},
 		},
 	},
