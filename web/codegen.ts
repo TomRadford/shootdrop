a09d312d9a3d4584423eec60d74298ea376eb2ed
Tom Radford <tom@theradford.com>
2025-06-11 08:40:07 +0200

    fix buggy typegen

--- web/codegen.ts
+++ web/codegen.ts
@@ -1,22 +1,25 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
 
+// ToDo: codegen is a little rough
 const config: CodegenConfig = {
 	schema: '../api/schema.ts',
-	documents: ['./**/*.{ts,tsx}', '!./__generated__/**'],
+	// documents: ['./**/*.{ts,tsx}', '!./__generated__/**'],
+	documents: ['./lib/apollo/queries.ts'],
 	generates: {
 		'./__generated__/graphql.ts': {
 			plugins: [
 				'typescript',
 				'typescript-operations',
-				'typescript-react-apollo',
-				'typescript-graphql-request',
+				// 'typescript-react-apollo',
+				// 'typescript-graphql-request',
 			],
 			config: {
-				withHooks: true,
-				withHOC: false,
-				withComponent: false,
+				// withHooks: true,
+				// withHOC: false,
+				// withComponent: false,
 				exportFragmentSpreadSubTypes: true,
 				dedupeFragments: true,
+				fragmentMasking: false,
 				skipTypename: false,
 				documentMode: 'documentNode',
 				strictScalars: true,
