c5b74bdaa9591a398339041f06dea28ae9e9ffe9
Tom Radford <tom@theradford.com>
2025-06-16 09:49:30 +0200

    fix: conditionally render loading title

--- web/components/Loading.tsx
+++ web/components/Loading.tsx
@@ -1,13 +1,13 @@
 import Layout from './layout'
 import LoadingSpinner from './elements/LoadingSpinner'
-const Loading = ({ title }: { title: string }) => {
+const Loading = ({ title }: { title?: string }) => {
 	return (
 		<Layout>
 			<div className="flex h-screen ">
 				<div className="m-auto text-center">
 					<div className="flex h-screen ">
 						<div className="m-auto flex flex-col items-center gap-3 text-center">
-							<p className="mx-4 text-sm text-gray-300">{title}</p>
+							{title && <p className="mx-4 text-sm text-gray-300">{title}</p>}
 							<LoadingSpinner />
 						</div>
 					</div>
