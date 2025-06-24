06ab8a7a0ed6028eb787fbe01aa28d78e0ee6a02
Tom Radford <tom@theradford.com>
2025-06-24 18:56:47 +0200

    chore: remove unused import

--- web/pages/gear/index.js
+++ web/pages/gear/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import GearBrowser from '../../components/gear/Browser'
-import ClientOnly from '../../components/ClientOnly'
 const GearPage = () => {
 	return (
 		<>
 			<Head>
-				<title>Browse Gear | Shoot Drop</title>
+				<title>Browse Gear | ShootDrop</title>
 			</Head>
 			<Layout>
 				<GearBrowser />
